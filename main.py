7ed0b5acfa1a466b1d593d5ed92e1ca76299444a
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2023-02-10 18:13:16 +0300

    Vizhener cipher commited

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from caesar_cipher import encode, decode
+from vizhener_cipher import encode_symb, decode_symb, encode_all, decode_all
 
 language = input('Language (RU/ENG): ')
 message = input('Your meassage: ').upper()
 step = int(input('Your step: '))
+key = input('Key word: ')
 
 if language == 'RU':
     alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 else:
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    encode_key = encode_symb(key)
+    enc_vizhener = encode_all(encode_key, encode_symb(message))
+    dec_vizhener = decode_all(encode_key, enc_vizhener)
+    print("To Vizhener cipher: ", ''.join(decode_symb(enc_vizhener)))
+    print("From Vizhener cipher: ", ''.join(decode_symb(dec_vizhener)))
 
-enc = encode(alphabet, message, step)
-dec = decode(alphabet, enc, step)
-print("To Caesar cipher: " + enc)
-print("From Caesar cipher: " + dec)
+enc_caesar = encode(alphabet, message, step)
+dec_caesar = decode(alphabet, enc_caesar, step)
+print("To Caesar cipher: " + enc_caesar)
+print("From Caesar cipher: " + dec_caesar)
--- vizhener_cipher.py
+++ vizhener_cipher.py
@@ -1,6 +1,26 @@
+from itertools import cycle
+
 def symbol_dict() -> dict:
     return dict([(i, chr(i)) for i in range(128)])
 
-def encode() -> list:
-    d = symbol_dict
-    return []
+def encode_symb(word: str) -> list:
+    d = symbol_dict()
+    return [key for w in word for key, value in d.items() if value == w]
+
+def compare(key: str, value: int) -> dict:
+    return dict([(index, [symbol[0], symbol[1]]) for index, symbol in enumerate(zip(value, cycle(key)))])
+
+def encode_all(key: str, value: str) -> list:
+    d = compare(key, value)
+    length = len(symbol_dict())
+    return [(v[0] + v[1]) % length for v in d.values()]
+
+def decode_symb(word: str) -> list:
+    # length = len(word)
+    d = symbol_dict()
+    return [d[symbol] for symbol in word if symbol in d]
+
+def decode_all(key: str, value: str):
+    d = compare(key, value)
+    length = len(symbol_dict())
+    return [(v[0] - v[1])%length for v in d.values()]
