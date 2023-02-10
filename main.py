0f5fa9a2ff579c20876112129de1189ca8632498
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2023-02-10 18:44:56 +0300

    Vizhener cipher fixed

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from caesar_cipher import encode, decode
-from vizhener_cipher import encode_symb, decode_symb, encode_all, decode_all
+from vizhener_cipher import generateKey, decryption, encryption
 
 language = input('Language (RU/ENG): ')
 message = input('Your meassage: ').upper()
 step = int(input('Your step: '))
 key = input('Key word: ')
+print()
 
 if language == 'RU':
     alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 else:
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    encode_key = encode_symb(key)
-    enc_vizhener = encode_all(encode_key, encode_symb(message))
-    dec_vizhener = decode_all(encode_key, enc_vizhener)
-    print("To Vizhener cipher: ", ''.join(decode_symb(enc_vizhener)))
-    print("From Vizhener cipher: ", ''.join(decode_symb(dec_vizhener)))
 
+    key = generateKey(message, key) 
+    encrypt_text = encryption(message,key) 
+    print("Encrypted message:", encrypt_text) 
+    print("Decrypted message:", decryption(encrypt_text, key)) 
+    
+print()
 enc_caesar = encode(alphabet, message, step)
 dec_caesar = decode(alphabet, enc_caesar, step)
 print("To Caesar cipher: " + enc_caesar)
--- vizhener_cipher.py
+++ vizhener_cipher.py
@@ -1,26 +1,24 @@
-from itertools import cycle
+def generateKey(string, key): 
+    key = list(key) 
+    if len(string) == len(key): 
+        return(key) 
+    else: 
+        for i in range(len(string) -len(key)): 
+            key.append(key[i % len(key)]) 
+    return("" . join(key)) 
+  
+def encryption(string, key): 
+    encrypt_text = [] 
+    for i in range(len(string)): 
+        x = (ord(string[i]) +ord(key[i])) % 26
+        x += ord('A') 
+        encrypt_text.append(chr(x)) 
+    return("" . join(encrypt_text)) 
 
-def symbol_dict() -> dict:
-    return dict([(i, chr(i)) for i in range(128)])
-
-def encode_symb(word: str) -> list:
-    d = symbol_dict()
-    return [key for w in word for key, value in d.items() if value == w]
-
-def compare(key: str, value: int) -> dict:
-    return dict([(index, [symbol[0], symbol[1]]) for index, symbol in enumerate(zip(value, cycle(key)))])
-
-def encode_all(key: str, value: str) -> list:
-    d = compare(key, value)
-    length = len(symbol_dict())
-    return [(v[0] + v[1]) % length for v in d.values()]
-
-def decode_symb(word: str) -> list:
-    # length = len(word)
-    d = symbol_dict()
-    return [d[symbol] for symbol in word if symbol in d]
-
-def decode_all(key: str, value: str):
-    d = compare(key, value)
-    length = len(symbol_dict())
-    return [(v[0] - v[1])%length for v in d.values()]
+def decryption(encrypt_text, key): 
+    orig_text = [] 
+    for i in range(len(encrypt_text)): 
+        x = (ord(encrypt_text[i]) -ord(key[i]) + 26) % 26
+        x += ord('A') 
+        orig_text.append(chr(x)) 
+    return("" . join(orig_text)) 
